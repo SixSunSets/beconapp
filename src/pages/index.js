8db1889d06cd14553fb19d88bdb8bef26f56330a
SixSunSets <alexander_now_8@hotmail.com>
2024-12-20 11:25:03 -0500

    modalupd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
-import Image from 'next/image';
+import Image from "next/image";
 import { Inter } from "next/font/google";
-import TopBar from './components/TopBar';
+import TopBar from "./components/TopBar";
 import { useTheme, useMediaQuery, Button } from "@mui/material";
-import Tabla from "../pages/components/Tabla"
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import Tabla from "../pages/components/Tabla";
+import TextField from "@mui/material/TextField";
 import socket from "./socket";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
@@ -14,11 +22,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const [openModal, setOpenModal] = useState(false);
   const [datos, setDatos] = useState([]);
   const [selectedData, setSelectedData] = useState([]); // Tabla <- Callback
-  console.log(selectedData)
+  console.log(selectedData);
   /*
   useEffect(() => {
     socket.on("actualizar_equipo", (data) => {
@@ -47,7 +55,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchDatos = async () => {
-      const response = await fetch("/datos_simulados.json"); 
+      const response = await fetch("/datos_simulados.json");
       const data = await response.json();
       setDatos(data);
     };
@@ -81,15 +89,31 @@ export default function Home() {
   };
 
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-start p-6 ${inter.className}`}>
+    <main
+      className={`flex min-h-screen flex-col items-center justify-start p-6 ${inter.className}`}
+    >
       <TopBar appname="AC Smart 5" />
       <TopBar appname="AC Smart 5" />
       <header className="w-full flex items-center mt-10">
-        <div className={`${isMobile ? 'flex' : 'flex-1'}`}>
-          <Image src="/images/logo.png" alt="Logo" width={isMobile ? 90 : 130} height={isMobile ? 90 : 130} />
+        <div className={`${isMobile ? "flex" : "flex-1"}`}>
+          <Image
+            src="/images/logo.png"
+            alt="Logo"
+            width={isMobile ? 90 : 130}
+            height={isMobile ? 90 : 130}
+          />
         </div>
-        <div className={`flex-1 flex ${isMobile ? 'justify-start' : 'justify-center'}`}>
-          <p className="font-bold text-lg md:text-xl lg:text-2xl" style={{ color: "#165b8d" }}>Control de equipos</p>
+        <div
+          className={`flex-1 flex ${
+            isMobile ? "justify-start" : "justify-center"
+          }`}
+        >
+          <p
+            className="font-bold text-lg md:text-xl lg:text-2xl"
+            style={{ color: "#165b8d" }}
+          >
+            Control de equipos
+          </p>
         </div>
         {!isMobile && <div className="flex-1"></div>}
       </header>
@@ -103,27 +127,7 @@ export default function Home() {
           Control Grupal
         </Button>
       </div>
-      <Tabla datos={datos} onSelectionChange={setSelectedData}/>
-      {/* Modal de Control Grupal */}
-      <Dialog open={openModal} onClose={handleCloseModal} fullWidth maxWidth="sm">
-        <DialogTitle>Control Grupal</DialogTitle>
-        <DialogContent>
-          {selectedData.map((row) => (
-            <div key={row.row_id} className="my-4">
-              <p><strong>Sede:</strong> {row.SEDE}</p>
-              <p><strong>Marca:</strong> {row.MARCA}</p>
-              <p><strong>Nombre:</strong> {row.NOMBRE}</p>
-              {/* Agrega controles aquí según lo que necesites */}
-            </div>
-          ))}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseModal}>Cancelar</Button>
-          <Button variant="contained" onClick={handleCommand}>
-            Comandar
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <Tabla datos={datos} onSelectionChange={setSelectedData} />
     </main>
   );
 }
